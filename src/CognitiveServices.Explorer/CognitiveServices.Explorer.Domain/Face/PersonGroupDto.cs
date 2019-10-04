758fa88db0cda41adc108cea86424e2d51a5a171
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-10-04 21:30:42 +1000

    Initial Face Api person groups design.

---
+++ src/CognitiveServices.Explorer/CognitiveServices.Explorer.Domain/Face/PersonGroupDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace CognitiveServices.Explorer.Domain.Face
+{
+    [DataContract]
+    public class PersonGroupDto
+    {
+        [DataMember(Name = "personGroupId")]
+        public string PersonGroupId { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "userData")]
+        public string UserData { get; set; }
+
+        [DataMember(Name = "recognitionModel")]
+        public string RecogntionModel { get; set; }
+    }
+}
