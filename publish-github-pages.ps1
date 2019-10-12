2f8f483c4c379f0d4abd0b363db9c9ba7172bbd0
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-10-12 15:49:34 +1100

    Updated publish script.

--- publish-github-pages.ps1
+++ publish-github-pages.ps1
@@ -4,16 +4,19 @@ $repoName = "CognitiveServices.Explorer"
 
 Write-Output "----==== Publish $pathToSolution"
 dotnet publish $pathToSolution -c Release -o ./dist/
+Write-Output ""
 
 Write-Output "----==== Copy from ./dist/$projectName/dist"
-Copy-Item -Path "./dist/$projectName/dist" -Destination "." -Recurse -Force
+Copy-Item -Path "./dist/$projectName/dist/*" -Destination "./" -Recurse -Force
+Write-Output ""
 
 $indexFile = "./index.html"
 $originalBaseUrlText = "<base href=""/"">";
 $targetBaseUrlText = "<base href=""/$repoName/"">";
 
 Write-Output "----==== Replace base href in $indexFile to be /$repoName/"
-((Get-Content -path $indexFile -Raw) -replace $originalBaseUrlText, $targetBaseUrlText) | Set-Content -Path $indexFile
+((Get-Content -path $indexFile -Raw) -replace $originalBaseUrlText, $targetBaseUrlText) | Set-Content -NoNewline -Path $indexFile
+Write-Output ""
 
 Write-Output "----==== Delete dist folder"
 Remove-Item ./dist/ -Recurse
