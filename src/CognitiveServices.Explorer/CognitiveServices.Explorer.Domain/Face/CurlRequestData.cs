a60f5f375251b507cb7fa12b8d2b0a5dbc5f096e
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-10-14 09:38:07 +1100

    Added hiding subscription key and style improvements.

---
+++ src/CognitiveServices.Explorer/CognitiveServices.Explorer.Domain/Face/CurlRequestData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CognitiveServices.Explorer.Domain.Face
+{
+    public class CurlRequestData
+    {
+        public string RequestName { get; set; }
+
+        public string Curl { get; set; }
+
+        public string SubscriptionKey { get; set; }
+
+        public string DisplayCurl(bool showSubscriptionKey)
+        {
+            string subscriptionKey = showSubscriptionKey ? SubscriptionKey : "***";
+            return Curl?.Replace("[token]", subscriptionKey ?? string.Empty);
+        }
+    }
+}
