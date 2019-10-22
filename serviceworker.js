7bbc36638aae6ab8213467dd4fd2b3df9c958bc9
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-10-22 23:37:35 +1000

    Removed offline support for now.

--- serviceworker.js
+++ serviceworker.js
@@ -77,12 +77,12 @@ if (self.location.hostname === 'localhost') {
 
 self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            console.log('[ServiceWorker] Caching app shell...');
-            return cache.addAll(filesToCache);
-        })
-    );
+    //e.waitUntil(
+    //    caches.open(cacheName).then(function (cache) {
+    //        console.log('[ServiceWorker] Caching app shell...');
+    //        return cache.addAll(filesToCache);
+    //    })
+    //);
 });
 
 self.addEventListener('activate', event => {
@@ -92,7 +92,8 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request, { ignoreSearch: true }).then(response => {
-            return response || fetch(event.request);
+            //return response || fetch(event.request);
+            return fetch(event.request);
         })
     );
 });
