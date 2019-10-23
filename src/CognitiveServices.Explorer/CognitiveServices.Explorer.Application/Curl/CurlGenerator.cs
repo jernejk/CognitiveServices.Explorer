229a24d7a0b29e4bb40390d12a989278f4e199a2
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-10-24 00:38:13 +1000

    Curl generator.

---
+++ src/CognitiveServices.Explorer/CognitiveServices.Explorer.Application/Curl/CurlGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace CognitiveServices.Explorer.Application.Curl
+{
+    public class CurlGenerator
+    {
+        public string Generate(HttpRequest request, CognitiveServiceConfig cognitiveServiceConfig, bool showToken = false)
+        {
+            var url = $"{cognitiveServiceConfig.BaseUrl}/{request.RelativePath}";
+            var curl = $"curl -X {request.HttpMethod.ToUpperInvariant()} \\";
+            curl += $"\n  '{url}' \\";
+            if (!string.IsNullOrWhiteSpace(request.ContentType))
+            {
+                curl += $"\n  -H 'Content-Type: {request.ContentType}' \\";
+            }
+
+            string token = showToken
+                ? cognitiveServiceConfig.Token
+                : "***";
+
+            curl += $"\n  -H '{request.TokenHeaderName}: {token}'";
+
+            return curl;
+        }
+    }
+}
