6617e2460648d628c05e8d257f8e1eebd33e135c
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-10-25 18:17:26 +1000

    Added support for Flurl.

---
+++ src/CognitiveServices.Explorer/CognitiveServices.Explorer.Web/Infrastructure/HttpClientFactoryForBlazor.cs
@@ -0,0 +1,14 @@
+﻿using Flurl.Http.Configuration;
+using Microsoft.AspNetCore.Blazor.Http;
+using System.Net.Http;
+
+namespace CognitiveServices.Explorer.Web.Infrastructure
+{
+    public class HttpClientFactoryForBlazor : DefaultHttpClientFactory
+    {
+        public override HttpMessageHandler CreateMessageHandler()
+        {
+            return new WebAssemblyHttpMessageHandler();
+        }
+    }
+}
