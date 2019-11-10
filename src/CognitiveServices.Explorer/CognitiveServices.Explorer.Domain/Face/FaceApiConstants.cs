b0c74518a4eab523f645fcd06456807d49394e7d
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-11-10 16:26:37 +1000

    Groups can be created with recognition model v2.

---
+++ src/CognitiveServices.Explorer/CognitiveServices.Explorer.Domain/Face/FaceApiConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace CognitiveServices.Explorer.Domain.Face
+{
+    public static class FaceApiConstants
+    {
+        public const string DetectModelV01 = "detection_01";
+        public const string DetectModelV02 = "detection_02";
+        public const string RecognitionModelV01 = "recognition_01";
+        public const string RecognitionModelV02 = "recognition_02";
+    }
+}
