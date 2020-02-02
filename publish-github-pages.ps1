b56f6e37a82473e7be45877dee87babd17569ef8
JK <jernej.kavka@outlook.com>
2020-02-02 18:29:52 +1100

    Fixed issues for Azure Blob Storage.

--- publish-github-pages.ps1
+++ publish-github-pages.ps1
@@ -17,6 +17,12 @@ $targetBaseUrlText = "<base href=""/$repoName/"">";
 Write-Output "----==== Replace base href in $indexFile to be /$repoName/"
 ((Get-Content -path $indexFile -Raw) -replace $originalBaseUrlText, $targetBaseUrlText) | Set-Content -Path $indexFile
 
+$indexFile = "./manifest.json"
+$originalBaseUrlText = """start_url"": ""/""";
+$targetBaseUrlText = """start_url"": ""/$repoName/""";
+
+Write-Output "----==== Replace manifest start URL in $indexFile to be /$repoName/"
+((Get-Content -path $indexFile -Raw) -replace $originalBaseUrlText, $targetBaseUrlText) | Set-Content -Path $indexFile
 
 Write-Output "----==== Delete dist folder"
 Remove-Item ./dist/ -Recurse
