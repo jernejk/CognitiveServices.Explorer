50168cad45427d957e2dec39345cfa3b5ead483e
JK <jernej.kavka@outlook.com>
2020-02-02 15:51:57 +1100

    Updated to support Azure Blob Storage and GitHub pages.

--- publish-github-pages.ps1
+++ publish-github-pages.ps1
@@ -10,5 +10,13 @@ Write-Output "----==== Copy from ./dist/$projectName/dist"
 Copy-Item -Path "./dist/$projectName/dist/*" -Destination "./" -Recurse -Force
 Write-Output ""
 
+$indexFile = "./index.html"
+$originalBaseUrlText = "<base href=""/"">";
+$targetBaseUrlText = "<base href=""/$repoName/"">";
+
+Write-Output "----==== Replace base href in $indexFile to be /$repoName/"
+((Get-Content -path $indexFile -Raw) -replace $originalBaseUrlText, $targetBaseUrlText) | Set-Content -Path $indexFile
+
+
 Write-Output "----==== Delete dist folder"
 Remove-Item ./dist/ -Recurse
