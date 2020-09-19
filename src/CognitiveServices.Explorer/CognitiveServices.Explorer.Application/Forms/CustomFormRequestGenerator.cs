e2318ea529c7e367dd113e521b4b0770c06cd87a
Jernej Kavka (JK) <JernejKavka@ssw.com.au>
2020-09-19 17:00:47 +1000

    Added Form Recognizer 2.1 Preview.1 support part 1.

--- src/CognitiveServices.Explorer/CognitiveServices.Explorer.Application/Forms/CustomFormRequestGenerator.cs
+++ src/CognitiveServices.Explorer/CognitiveServices.Explorer.Application/Forms/CustomFormRequestGenerator.cs
@@ -6,20 +6,28 @@ namespace CognitiveServices.Explorer.Application.Forms
 {
     public class CustomFormRequestGenerator
     {
-        public static HttpRequest GetModels(bool showSummary = true)
+        public const string StableVersion = "2.0";
+        public const string PreviewVersion = "2.1-preview.1";
+
+        public static HttpRequest GetModels(bool showSummary = true, string version = StableVersion)
         {
             return new HttpRequest
             {
                 HttpMethod = HttpMethods.Get,
                 ContentType = "application/json",
-                RelativePath = "formrecognizer/v2.0/custom/models",
+                RelativePath = $"formrecognizer/{version}/custom/models",
                 Cost = ServiceCost.FormApiTransaction(1),
                 Queries = new Dictionary<string, string> { { "op", showSummary.ToString() } },
-                CognitiveServiceDoc = "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/GetCustomModels"
+                CognitiveServiceDoc = version switch
+                {
+                    StableVersion => "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/GetCustomModels",
+                    PreviewVersion => "https://westcentralus.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2-1-preview-1/operations/GetCustomModels",
+                    _ => ""
+                }
             };
         }
 
-        public static HttpRequest StartAnalyzeForm(string modelId, byte[] data, string contentType, bool includeTextDetails = true)
+        public static HttpRequest StartAnalyzeForm(string modelId, byte[] data, string contentType, bool includeTextDetails = true, string version = StableVersion)
         {
             // Content type should be image/jpeg, image/png, image/tiff or application/pdf.
             return new HttpRequest
@@ -27,35 +35,50 @@ namespace CognitiveServices.Explorer.Application.Forms
                 HttpMethod = HttpMethods.Post,
                 BinaryContent = data,
                 ContentType = contentType,
-                RelativePath = $"formrecognizer/v2.0/custom/models/{modelId}/analyze",
+                RelativePath = $"formrecognizer/{version}/custom/models/{modelId}/analyze",
                 Queries = new Dictionary<string, string> { { "includeTextDetails", includeTextDetails.ToString() } },
                 Cost = ServiceCost.FormApiTransaction(1),
-                CognitiveServiceDoc = "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/AnalyzeWithCustomForm"
+                CognitiveServiceDoc = version switch
+                {
+                    StableVersion => "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/AnalyzeWithCustomForm",
+                    PreviewVersion => "https://westcentralus.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2-1-preview-1/operations/GetAnalyzeReceiptResult",
+                    _ => ""
+                }
             };
         }
 
-        public static HttpRequest StartAnalyzeForm(string modelId, string imageUrl, bool includeTextDetails = true)
+        public static HttpRequest StartAnalyzeForm(string modelId, string imageUrl, bool includeTextDetails = true, string version = StableVersion)
         {
             return new HttpRequest
             {
                 HttpMethod = HttpMethods.Post,
                 Body = JsonSerializer.Serialize(new { source = imageUrl }),
                 ContentType = "application/json",
-                RelativePath = $"formrecognizer/v2.0/custom/models/{modelId}/analyze",
+                RelativePath = $"formrecognizer/{version}/custom/models/{modelId}/analyze",
                 Queries = new Dictionary<string, string> { { "includeTextDetails", includeTextDetails.ToString() } },
                 Cost = ServiceCost.FormApiTransaction(1),
-                CognitiveServiceDoc = "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/AnalyzeWithCustomForm"
+                CognitiveServiceDoc = version switch
+                {
+                    StableVersion => "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/AnalyzeWithCustomForm",
+                    PreviewVersion=> "https://westcentralus.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2-1-preview-1/operations/GetAnalyzeReceiptResult",
+                    _ => ""
+                }
             };
         }
 
-        public static HttpRequest GetResultFromForm(string operationLocation)
+        public static HttpRequest GetResultFromForm(string operationLocation, string version = StableVersion)
         {
             return new HttpRequest
             {
                 HttpMethod = HttpMethods.Get,
                 Cost = ServiceCost.FormApiTransaction(0),
                 AbsoluteUrl = operationLocation,
-                CognitiveServiceDoc = "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/GetAnalyzeFormResult"
+                CognitiveServiceDoc = version switch
+                {
+                    StableVersion => "https://westus2.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2/operations/GetAnalyzeFormResult",
+                    PreviewVersion => "https://westcentralus.dev.cognitive.microsoft.com/docs/services/form-recognizer-api-v2-1-preview-1/operations/GetAnalyzeFormResult",
+                    _ => ""
+                }
             };
         }
     }
